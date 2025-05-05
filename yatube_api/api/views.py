e62ff482af73725d3173c1de1ae2c0ff37b71edc
RussianBear0 <vova.denisov.19982@mail.ru>
2025-05-05 18:08:41 +0300

    Add files via upload

---
+++ yatube_api/api/views.py
@@ -0,0 +1,78 @@
+# TODO:  Напишите свой вариант
+from django.shortcuts import get_object_or_404
+from django_filters.rest_framework import DjangoFilterBackend
+from posts.models import Post, Group
+from rest_framework import filters, permissions, viewsets
+from django.core.exceptions import PermissionDenied
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+from .serializers import (PostSerializer, CommentSerializer,
+                          FollowSerializer, GroupSerializer)
+from .permissions import IsAuthorOrReadOnly
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+    filter_backends = [DjangoFilterBackend]
+    filter_fields = ['group']
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    def perform_update(self, serializer):
+        if serializer.instance.author != self.request.user:
+            raise PermissionDenied("Изменение чужого контента запрещено!")
+        return super(PostViewSet, self).perform_update(serializer)
+
+    def perform_destroy(self, serializer):
+        if serializer.author != self.request.user:
+            raise PermissionDenied("Удаление контента запрещено!")
+        return super(PostViewSet, self).perform_destroy(serializer)
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+
+    def get_queryset(self):
+        post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
+        return post.comments
+
+    def perform_create(self, serializer):
+        post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
+        serializer.save(author=self.request.user, post=post)
+
+    def perform_update(self, serializer):
+        if serializer.instance.author != self.request.user:
+            raise PermissionDenied("Изменение чужого контента запрещено!")
+        return super(CommentViewSet, self).perform_update(serializer)
+
+    def perform_destroy(self, serializer):
+        if serializer.author != self.request.user:
+            raise PermissionDenied("Удаление контента запрещено!")
+        return super(CommentViewSet, self).perform_destroy(serializer)
+
+
+class FollowViewSet(viewsets.ModelViewSet):
+
+    serializer_class = FollowSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['user__username', 'following__username']
+
+    def get_queryset(self):
+        user = self.request.user
+        return user.follower.all()
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
