eaba103113ec547c7a6afdcb61d657ba5f65879c
RussianBear0 <vova.denisov.19982@mail.ru>
2025-05-05 20:14:41 +0300

    Add files via upload

---
+++ yatube_api/api/serializers.py
@@ -0,0 +1,73 @@
+from rest_framework import serializers
+from rest_framework.relations import SlugRelatedField
+import base64
+from django.core.files.base import ContentFile
+from posts.models import Comment, Post, User, Follow, Group
+from rest_framework.validators import UniqueTogetherValidator
+
+
+class Base64ImageField(serializers.ImageField):
+    def to_internal_value(self, data):
+        if isinstance(data, str) and data.startswith('data:image'):
+            format, imgstr = data.split(';base64,')
+            ext = format.split('/')[-1]
+            data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
+
+        return super().to_internal_value(data)
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author = SlugRelatedField(slug_field='username', read_only=True)
+    image = Base64ImageField(required=False, allow_null=True)
+
+    class Meta:
+        fields = '__all__'
+        model = Post
+
+    def create(self, validated_data):
+        return super().create(validated_data)
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True, slug_field='username'
+    )
+    post = serializers.ReadOnlyField(source='post.id')
+
+    class Meta:
+        fields = '__all__'
+        model = Comment
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True,
+        default=serializers.CurrentUserDefault()
+    )
+    following = serializers.SlugRelatedField(
+        slug_field='username',
+        queryset=User.objects.all()
+    )
+
+    class Meta:
+        fields = ('user', 'following')
+        model = Follow
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=['user', 'following']
+            )
+        ]
+
+    def validate(self, data):
+        if self.context['request'].user != data.get('following'):
+            return data
+        raise serializers.ValidationError("Самоподписка запрещена")
+
+
+class GroupSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        fields = '__all__'
+        model = Group
