e1aef7b1681f72fb240d097c99a2e919630dccab
RussianBear0 <vova.denisov.19982@mail.ru>
2025-05-29 09:58:14 +0300

    Update admin.py

--- yatube_api/posts/admin.py
+++ yatube_api/posts/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 
-from .models import Post
+from .models import Post, Group, Comment, Follow
+
+
+class PostInline(admin.StackedInline):
+    model = Post
+    extra = 0
+
+
+class GroupAdmin(admin.ModelAdmin):
+    inlines = (
+        PostInline,
+    )
+
 
 class PostAdmin (admin.ModelAdmin):
     model = Post
@@ -12,3 +24,18 @@ class PostAdmin (admin.ModelAdmin):
     list_editable = ("group",)
     search_fields = ('text',)
     list_filter = ('pub_date',)
+
+
+class GroupAdmin (admin.ModelAdmin):
+    
+
+class CommentAdmin (admin.ModelAdmin):
+
+
+class FollowAdmin (admin.ModelAdmin):
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Group, GroupAdmin)
+admin.site.register(Comment)
+admin.site.register(Follow) 
